4afaf08d3567c4cf0622844686ac8b227f78101a
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-03-23 17:43:30 +0100

    add install phase

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-rm -rf build_gcc ; mkdir build_gcc ; cd build_gcc ; cmake .. ; cmake --build . ; cd ..
+rm -rf build_gcc ; mkdir build_gcc ; cd build_gcc ; cmake .. ; cmake --build . --target install ; cd ..
