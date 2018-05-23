cdee910c08e6b188e4b660a9f6f02ca943c25cc0
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-05-23 18:38:32 +0200

    improve compatibility with gcc-8

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
-rm -rf build
-mkdir build
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* && "$1" == "gcc" ]]; then
+  export CC="/usr/local/bin/gcc-8"
+  export CXX="/usr/local/bin/g++-8"
+fi
+
+mkdir -p build
 cd build
-cmake .. -G "Ninja"
+cmake ..
+#cmake .. -G "Ninja"
 cmake --build . --target install
 cd ..
